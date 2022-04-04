22dbf4a47d6ce7681cd0d0c569801e5abb28f89d
yojessie <sprout.jessie@gmail.com>
2022-04-04 22:42:42 +0900

    🚧  Implement deleting all search history items

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -1,13 +1,23 @@
 const gnbSearch = document.querySelector('.gnb-search')
 const gnbSearchInput = gnbSearch.querySelector('.form-input')
 const gnbSearchHistory = gnbSearch.querySelector('.search-history')
+const gnbSearchHistoryList = gnbSearchHistory.querySelector('ol')
 
+const deleteAllButton = gnbSearchHistory.querySelector(
+  '.search-history-header button'
+)
+
+// 바깥영역 클릭 시 히스토리 창 닫힘
 function openSearchGnbHistory() {
-  gnbSearchHistory.classList.add('is-active')
+  if (gnbSearchHistoryList.children.length == 0) {
+    return // 리스트에 내용 삭제된 이후면 뒤에 코드 실행하지 않음
+  }
 
   if (gnbSearchHistory.classList.contains('is-active')) {
     window.addEventListener('click', closeSearchGnbHistory)
   }
+
+  gnbSearchHistory.classList.add('is-active')
 }
 
 function closeSearchGnbHistory(e) {
@@ -18,3 +28,11 @@ function closeSearchGnbHistory(e) {
 }
 
 gnbSearchInput.addEventListener('focus', openSearchGnbHistory)
+
+// 전체삭제 버튼 클릭 시 리스트 삭제
+function deleteAllSearchHistory() {
+  gnbSearchHistoryList.innerHTML = ''
+  gnbSearchHistory.classList.remove('is-active')
+}
+
+deleteAllButton.addEventListener('click', deleteAllSearchHistory)
