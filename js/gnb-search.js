e633418e8fec8fdfba407ea89fca4b07fc06e484
yojessie <sprout.jessie@gmail.com>
2022-04-07 22:51:29 +0900

    🚧  Implement deleting individual search history iitem

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -7,32 +7,54 @@ const deleteAllButton = gnbSearchHistory.querySelector(
   '.search-history-header button'
 )
 
+const deleteButton = gnbSearchHistoryList.querySelectorAll('.delete-button')
+
+// 히스토리 모달 닫은 후 윈도우 클릭 이벤트 삭제해주는 코드를 별도 함수로 만들어 활용
+function afterCloseSearchHistory() {
+  gnbSearchHistory.classList.remove('is-active')
+  window.removeEventListener('click', closeSearchGnbHistory)
+}
+
 // 바깥영역 클릭 시 히스토리 창 닫힘
+function closeSearchGnbHistory(e) {
+  if (!gnbSearch.contains(e.target)) {
+    afterCloseSearchHistory()
+  }
+}
+
 function openSearchGnbHistory() {
   if (gnbSearchHistoryList.children.length == 0) {
     return // 리스트에 내용 삭제된 이후면 뒤에 코드 실행하지 않음
   }
-
+  gnbSearchHistory.classList.add('is-active')
   if (gnbSearchHistory.classList.contains('is-active')) {
     window.addEventListener('click', closeSearchGnbHistory)
-  }
-
-  gnbSearchHistory.classList.add('is-active')
-}
-
-function closeSearchGnbHistory(e) {
-  if (!gnbSearch.contains(e.target)) {
-    gnbSearchHistory.classList.remove('is-active')
-    window.removeEventListener('click', closeSearchGnbHistory)
   }
 }
 
 gnbSearchInput.addEventListener('focus', openSearchGnbHistory)
+// window.addEventListener('click', closeSearchGnbHistory)
 
 // 전체삭제 버튼 클릭 시 리스트 삭제
 function deleteAllSearchHistory() {
   gnbSearchHistoryList.innerHTML = ''
-  gnbSearchHistory.classList.remove('is-active')
+  afterCloseSearchHistory()
 }
 
 deleteAllButton.addEventListener('click', deleteAllSearchHistory)
+
+// 리스트 하나씩 삭제
+function deleteSearchHistoryItem(e) {
+  e.stopPropagation() // 이벤트 버블링을 막음
+  const itemToDelete = this.parentNode
+  gnbSearchHistoryList.removeChild(itemToDelete)
+  // DOM에서 요소를 삭제할때는 부모요소에서 메소드를 사용해 삭제 해야한다.
+
+  if (gnbSearchHistoryList.children.length == 0) {
+    afterCloseSearchHistory()
+  }
+}
+
+deleteButton.forEach(function (button) {
+  button.addEventListener('click', deleteSearchHistoryItem)
+})
