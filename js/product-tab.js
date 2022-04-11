8c0af5a6dc7afd94728f2133cc44ecf3aac46842
yojessie <sprout.jessie@gmail.com>
2022-04-11 23:28:18 +0900

    💫  Scroll to tab-pannel when product-tab-item is clicked

--- js/product-tab.js
+++ js/product-tab.js
@@ -1,6 +1,9 @@
 const productTab = document.querySelector('.product-tab')
 const productTabButtonList = productTab.querySelectorAll('button')
 
+const TOP_HEADER_DESKTOP = 80 + 50 + 54
+const TOP_HEADER_MOBILE = 50 + 40 + 40
+
 let currentActiveTab = productTab.querySelector('.is-active')
 
 function activateTab() {
@@ -10,6 +13,23 @@ function activateTab() {
   tabItem.classList.add('is-active')
 }
 
+// 탭 메뉴 클릭 시 해당 영역으로 이동
+function scrollToTabPannel() {
+  const tabPanelId = this.parentNode.getAttribute('aria-labelledby')
+  const tabPanel = document.querySelector(`#${tabPanelId}`)
+
+  const scrollAmount =
+    tabPanel.getBoundingClientRect().top -
+    (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP : TOP_HEADER_MOBILE)
+  // 윈도우 가로값이 768보다 크니? 그럼 데스크탑만큼 동작하고 : 아니면 모바일만큼 동작해
+
+  window.scrollBy({
+    top: scrollAmount,
+    behavior: 'smooth', // 스르륵 움직여라~ 사파리에서 동작 안함...
+  })
+}
+
 productTabButtonList.forEach((button) => {
   button.addEventListener('click', activateTab)
+  button.addEventListener('click', scrollToTabPannel)
 })
